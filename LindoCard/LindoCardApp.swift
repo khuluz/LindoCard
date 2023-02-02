f6e0a7416247cfbd0bdde1148e7c422bce64b10c
khuluz <lindokuhle.zungu@outlook.com>
2023-02-02 18:29:51 +0200

    Initial Commit

---
+++ LindoCard/LindoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LindoCardApp.swift
+//  LindoCard
+//
+//  Created by Lindo on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct LindoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
